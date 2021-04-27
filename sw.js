3de8bf7f301ab89992e6375d79e5d0f7f4810b8e
dodain <dodain@pop-os.localdomain>
2021-04-27 21:48:44 +0500

    First blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210421.2103';
+const cacheName = 'chirpy-20210428.0047';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
